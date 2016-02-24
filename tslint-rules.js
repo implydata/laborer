239adf48514169604f64a319591fd830fffa06a8
Vadim Ogievetsky <vadimon@gmail.com>
2016-02-24 15:13:50 -0800

    updated rules

--- tslint-rules.js
+++ tslint-rules.js
@@ -22,11 +22,14 @@ module.exports = {
     "label-position": true,
     "label-undefined": true,
     "max-line-length": [true, 200],
+    "member-access": false,
     "member-ordering": [true,
       "static-before-instance"
     ],
+    "no-any": false,
     "no-arg": true,
     "no-bitwise": false,
+    "no-consecutive-blank-lines": false,
     "no-console": [true,
       "debug",
       "info",
@@ -41,36 +44,52 @@ module.exports = {
     "no-duplicate-variable": false,
     "no-empty": false,
     "no-eval": true,
+    "no-inferrable-types": true,
+    "no-internal-module": false,
+    "no-null-keyword": false,
+    "no-require-imports": false, // ?
+    "no-shadowed-variable": false,
     "no-string-literal": false,
-    "no-switch-case-fall-through": true,
-    "no-trailing-comma": true,
+    "no-switch-case-fall-through": false, // TS 1.8 takes care of that.
     "no-trailing-whitespace": true,
+    "no-unreachable": false, // TS 1.8 takes care of that.
     "no-unused-expression": false,
     "no-unused-variable": false,
-    "no-unreachable": true,
     "no-use-before-declare": true,
+    "no-var-keyword": false,
     "no-var-requires": false,
+    "object-literal-sort-keys": false,
     "one-line": [true,
-      "check-open-brace",
       "check-catch",
       "check-else",
+      "check-finally",
+      "check-open-brace",
       "check-whitespace"
     ],
-    "quotemark": [false, "single"],
+    "quotemark": [false, "double", "jsx-double", "avoid-escape"],
     "radix": true,
     "semicolon": true,
+    "switch-default": false,
+    "trailing-comma": [true, {
+      "singleline": "never",
+      "multiline": "never"
+    }],
     "triple-equals": [true, "allow-null-check"],
-    "typedef": [true,
-      "callSignature"
-    ],
-    "typedef-whitespace": [true,
-      ["callSignature", "noSpace"],
-      ["catchClause", "noSpace"],
-      ["indexSignature", "space"]
-    ],
-    "use-strict": [true,
-      "check-module"
-    ],
+    "typedef": false,
+    "typedef-whitespace": [true, {
+      "call-signature": "nospace",
+      "index-signature": "nospace",
+      "parameter": "nospace",
+      "property-declaration": "nospace",
+      "variable-declaration": "nospace"
+    }, {
+      "call-signature": "onespace",
+      "index-signature": "onespace",
+      "parameter": "onespace",
+      "property-declaration": "onespace",
+      "variable-declaration": "onespace"
+    }],
+    "use-strict": false, // TS 1.8 takes care of that.
     "variable-name": false,
     "whitespace": [true,
       "check-branch",
